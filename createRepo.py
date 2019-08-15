8c7a75bf3535a8050b93bedf47f8947fb30c1eca
LeoLYW12138 <leo62227@gmail.com>
2019-08-15 23:53:38 +0800

    Delete createRepo.py

--- createRepo.py
+++
@@ -1,8 +0,0 @@
-from github import Github
-import sys
-
-repoName = str(sys.argv[1])
-g = Github("Your access token")  # access token
-u = g.get_user()
-repo = u.create_repo(repoName)
-print(f"Successfully created a repository named {repoName}")
