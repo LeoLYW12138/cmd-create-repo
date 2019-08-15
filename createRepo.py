cc69de9bf7583d05165f03b607767decbc0ca697
LeoLYW12138 <leo62227@gmail.com>
2019-08-15 23:54:54 +0800

    update

--- createRepo.py
+++ createRepo.py
@@ -2,7 +2,7 @@ from github import Github
 import sys
 
 repoName = str(sys.argv[1])
-g = Github("6653643be0173ee9e780d1db476ebb7a94e32553")  # access token
+g = Github("Your access token")  # access token
 u = g.get_user()
 repo = u.create_repo(repoName)
 print(f"Successfully created a repository named {repoName}")
